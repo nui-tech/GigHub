78ece780675abbeb001a5e8040cc5425023ef973
Nui Rattapon <contact@nuirattapon.com>
2017-07-06 19:56:19 +1200

    Imprement tation saving a gig

--- GigHub/Models/Gig.cs
+++ GigHub/Models/Gig.cs
@@ -7,8 +7,11 @@ namespace GigHub.Models
     {
         public int Id { get; set; }
 
-        [Required]
+
         public ApplicationUser Artist { get; set; }
+
+        [Required]
+        public string ArtistId { get; set; }
 
         public DateTime DateTime { get; set; }
 
@@ -17,6 +20,8 @@ namespace GigHub.Models
         public string Venue { get; set; }
 
         [Required]
+        public byte GenreId { get; set; }
+
         public Genre Genre { get; set; }
     }
 
